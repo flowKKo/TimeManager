c8df673cda15c7200891fa6fd83d5e717b29930d
AgarthaSF <1426887306@qq.com>
2022-03-24 19:28:20 +0800

    first

---
+++ CalendarView_modified/app/src/main/java/com/haibin/calendarviewproject/custom/CustomActivity.java
@@ -0,0 +1,163 @@
+package com.haibin.calendarviewproject.custom;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.haibin.calendarview.Calendar;
+import com.haibin.calendarview.CalendarLayout;
+import com.haibin.calendarview.CalendarView;
+import com.haibin.calendarviewproject.R;
+import com.haibin.calendarviewproject.base.activity.BaseActivity;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomActivity extends BaseActivity implements
+        CalendarView.OnCalendarSelectListener,
+        CalendarView.OnYearChangeListener,
+        View.OnClickListener {
+
+    TextView mTextMonthDay;
+
+    TextView mTextYear;
+
+    TextView mTextLunar;
+
+    TextView mTextCurrentDay;
+
+    CalendarView mCalendarView;
+
+    RelativeLayout mRelativeTool;
+    private int mYear;
+    CalendarLayout mCalendarLayout;
+
+    public static void show(Context context) {
+        context.startActivity(new Intent(context, CustomActivity.class));
+    }
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_custom;
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void initView() {
+        setStatusBarDarkMode();
+        mTextMonthDay = findViewById(R.id.tv_month_day);
+        mTextYear = findViewById(R.id.tv_year);
+        mTextLunar = findViewById(R.id.tv_lunar);
+        mRelativeTool = findViewById(R.id.rl_tool);
+        mCalendarView =  findViewById(R.id.calendarView);
+        mTextCurrentDay =  findViewById(R.id.tv_current_day);
+        mTextMonthDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mCalendarLayout.isExpand()) {
+                    mCalendarLayout.expand();
+                    return;
+                }
+                mCalendarView.showYearSelectLayout(mYear);
+                mTextLunar.setVisibility(View.GONE);
+                mTextYear.setVisibility(View.GONE);
+                mTextMonthDay.setText(String.valueOf(mYear));
+            }
+        });
+        findViewById(R.id.fl_current).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCalendarView.scrollToCurrent();
+            }
+        });
+        mCalendarLayout = findViewById(R.id.calendarLayout);
+        mCalendarView.setOnCalendarSelectListener(this);
+        mCalendarView.setOnYearChangeListener(this);
+        mTextYear.setText(String.valueOf(mCalendarView.getCurYear()));
+        mYear = mCalendarView.getCurYear();
+        mTextMonthDay.setText(mCalendarView.getCurMonth() + "月" + mCalendarView.getCurDay() + "日");
+        mTextLunar.setText("今日");
+        mTextCurrentDay.setText(String.valueOf(mCalendarView.getCurDay()));
+    }
+
+    @Override
+    protected void initData() {
+        int year = mCalendarView.getCurYear();
+        int month = mCalendarView.getCurMonth();
+
+        Map<String, Calendar> map = new HashMap<>();
+        map.put(getSchemeCalendar(year, month, 3, 0xFF40db25, "假").toString(),
+                getSchemeCalendar(year, month, 3, 0xFF40db25, "假"));
+        map.put(getSchemeCalendar(year, month, 6, 0xFFe69138, "事").toString(),
+                getSchemeCalendar(year, month, 6, 0xFFe69138, "事"));
+        map.put(getSchemeCalendar(year, month, 9, 0xFFdf1356, "议").toString(),
+                getSchemeCalendar(year, month, 9, 0xFFdf1356, "议"));
+        map.put(getSchemeCalendar(year, month, 13, 0xFFedc56d, "记").toString(),
+                getSchemeCalendar(year, month, 13, 0xFFedc56d, "记"));
+        map.put(getSchemeCalendar(year, month, 14, 0xFFedc56d, "记").toString(),
+                getSchemeCalendar(year, month, 14, 0xFFedc56d, "记"));
+        map.put(getSchemeCalendar(year, month, 15, 0xFFaacc44, "假").toString(),
+                getSchemeCalendar(year, month, 15, 0xFFaacc44, "假"));
+        map.put(getSchemeCalendar(year, month, 18, 0xFFbc13f0, "记").toString(),
+                getSchemeCalendar(year, month, 18, 0xFFbc13f0, "记"));
+        map.put(getSchemeCalendar(year, month, 25, 0xFF13acf0, "假").toString(),
+                getSchemeCalendar(year, month, 25, 0xFF13acf0, "假"));
+        map.put(getSchemeCalendar(year, month, 27, 0xFF13acf0, "多").toString(),
+                getSchemeCalendar(year, month, 27, 0xFF13acf0, "多"));
+        //此方法在巨大的数据量上不影响遍历性能，推荐使用
+        mCalendarView.setSchemeDate(map);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        CustomActivity.show(this);
+    }
+
+    private Calendar getSchemeCalendar(int year, int month, int day, int color, String text) {
+        Calendar calendar = new Calendar();
+        calendar.setYear(year);
+        calendar.setMonth(month);
+        calendar.setDay(day);
+        calendar.setSchemeColor(color);//如果单独标记颜色、则会使用这个颜色
+        calendar.setScheme(text);
+        calendar.addScheme(new Calendar.Scheme());
+        calendar.addScheme(0xFF008800, "假");
+        calendar.addScheme(0xFF008800, "节");
+        return calendar;
+    }
+
+    @Override
+    public void onCalendarOutOfRange(Calendar calendar) {
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onCalendarSelect(Calendar calendar, boolean isClick) {
+        mTextLunar.setVisibility(View.VISIBLE);
+        mTextYear.setVisibility(View.VISIBLE);
+        mTextMonthDay.setText(calendar.getMonth() + "月" + calendar.getDay() + "日");
+        mTextYear.setText(String.valueOf(calendar.getYear()));
+        mTextLunar.setText(calendar.getLunar());
+        mYear = calendar.getYear();
+
+        Log.e("onDateSelected", "  -- " + calendar.getYear() +
+                "  --  " + calendar.getMonth() +
+                "  -- " + calendar.getDay() +
+                "  --  " + isClick + "  --   " + calendar.getScheme());
+    }
+
+    @Override
+    public void onYearChange(int year) {
+        mTextMonthDay.setText(String.valueOf(year));
+    }
+}
---
+++ CalendarView_modified/app/src/main/java/com/haibin/calendarviewproject/textActivity.java
@@ -0,0 +1,19 @@
+package com.haibin.calendarviewproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.haibin.calendarviewproject.full.FullActivity;
+
+public class textActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text);
+
+    }
+}
