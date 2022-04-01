cd1c62761edd9f828794d9e6cbeb2c67fdb0ca72
YnmtDJ <1624309321@qq.com>
2022-04-01 22:17:16 +0800

    AddToDialog ver2

--- ToDoList/app/src/main/java/com/example/todolist/MainActivity.java
+++ ToDoList/app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,48 +2,47 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DatePickerDialog;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
-import android.graphics.ColorSpace;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-
-import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import org.litepal.LitePal;
-import org.litepal.tablemanager.Connector;
+import org.litepal.crud.DataSupport;
+import org.litepal.crud.LitePalSupport;
 
-import java.util.Calendar;
-import java.util.Locale;
-
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     private BottomSheet_Add bottomSheet_add=null;//添加待办事项对话框
+    private AddTodoDialog addTodoDialog=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LitePal.getDatabase();//创建数据库
+        LitePal.getDatabase();//创建/连接数据库
+        //LitePal.deleteAll("Todo");
     }
 
     public void onClick(View view){//监听函数
         switch (view.getId()){
             case R.id.button_add://添加待办事项按钮
-                //BottomSheetDialog dialog=new BottomSheetDialog();
-                bottomSheet_add=new BottomSheet_Add(this);
-                //bottomSheet_add.getWindow().getDecorView().setBackgroundColor(Color.TRANSPARENT);
-                //bottomSheet_add.getWindow().getDecorView().setBackground(null);
-                bottomSheet_add.show();
-                //bottomSheet_add.getWindow().getDecorView().setBackground(null); // 移除dialog的decorview背景色
+                /*bottomSheet_add=new BottomSheet_Add(this);
+                bottomSheet_add.show();*/
+
+                addTodoDialog=new AddTodoDialog();
+                addTodoDialog.show(getSupportFragmentManager(),"This is addTodoDialog");
+
+
+//                RepeatSetDialog dialog=new RepeatSetDialog();
+//                dialog.show(getSupportFragmentManager(),"this is tag");
+
+               /* DialogFragment dialog=new DialogFragment(R.layout.repeat_set);
+                dialog.show(getSupportFragmentManager(),"this is tag");*/
                 break;
             default:
+                List<Todo> list= LitePal.findAll(Todo.class);
+                list=list;
             break;
         }
     }
--- ToDoList/app/src/main/java/com/example/todolist/ToDo.java
+++ ToDoList/app/src/main/java/com/example/todolist/ToDo.java
@@ -1,6 +1,7 @@
 package com.example.todolist;
 
 import org.litepal.LitePal;
+import org.litepal.annotation.Column;
 import org.litepal.crud.DataSupport;
 import org.litepal.crud.LitePalSupport;
 import org.litepal.exceptions.DataSupportException;
@@ -14,22 +15,120 @@ class Date{
         this.month=month;
         this.day=day;
     }
+    void SetDate(int year,int month,int day) {
+        this.year=year;
+        this.month=month;
+        this.day=day;
+    }
 
+    void increase(){//自增一天
+        day++;
+        if(month==2){//2月
+            if(year%4==0){//闰年
+                if(day==30){
+                    month++;
+                    day=1;
+                }
+            }
+            else{//非闰年
+                if(day==29){
+                    month++;
+                    day=1;
+                }
+            }
+        }
+        else if((month%2==0&&month<8)||(month%2==1&&month>=8)){//30天
+            if(day==31){
+                month++;
+                day=1;
+            }
+        }
+        else{//31天
+            if(day==32){
+                month++;
+                day=1;
+            }
+        }
+        if(month==13){
+            month=1;
+            year++;
+        }
+    }
+
+    boolean LessEqual(Date rhs){//日期比较
+        if(year<rhs.year) return true;
+        else if(year> rhs.year) return false;
+        if(month<rhs.month) return true;
+        else if(month>rhs.month) return false;
+        if(day<=rhs.day) return true;
+        else return false;
+    }
+
+    public String tostring(){//转为字符串
+        return Integer.toString(year)+'/'+
+                String.format("%02d",month)+'/'+
+                String.format("%02d",day);
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
 }
 
 class Time{
     int hour;//小时
     int minute;//分钟
+
+    public String tostring(){
+        return String.format("%02d",hour)+':'+
+                String.format("%02d",minute);
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
 }
 
 
-public class ToDo extends LitePalSupport {
+public class Todo extends LitePalSupport {
     private int id;//todo的id
     private String todo;//待办事项
-    private Date date;//待办事项时间
-    private Date create_date;//todo创建时间
+    private String date;//待办事项时间
+    private String create_date;//todo创建时间
     private boolean is_clock;//是否提醒
-    private Time time;//提醒时间
+    private String time;//提醒时间
 
     public void setId(int id) {
         this.id = id;
@@ -39,11 +138,11 @@ public class ToDo extends LitePalSupport {
         this.todo = todo;
     }
 
-    public void setDate(Date date) {
+    public void setDate(String date) {
         this.date = date;
     }
 
-    public void setCreate_date(Date create_date) {
+    public void setCreate_date(String create_date) {
         this.create_date = create_date;
     }
 
@@ -51,7 +150,7 @@ public class ToDo extends LitePalSupport {
         this.is_clock = is_clock;
     }
 
-    public void setTime(Time time) {
+    public void setTime(String time) {
         this.time = time;
     }
 
@@ -63,11 +162,11 @@ public class ToDo extends LitePalSupport {
         return todo;
     }
 
-    public Date getDate() {
+    public String getDate() {
         return date;
     }
 
-    public Date getCreate_date() {
+    public String getCreate_date() {
         return create_date;
     }
 
@@ -75,7 +174,7 @@ public class ToDo extends LitePalSupport {
         return is_clock;
     }
 
-    public Time getTime() {
+    public String getTime() {
         return time;
     }
 }
